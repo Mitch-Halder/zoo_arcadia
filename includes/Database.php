e1c0dd439868e6ec1ff11aa7f8f1a9539fa08e3f
Mitch <halder.jean-michel@orange.fr>
2025-02-18 23:21:46 +0100

    Update Database.php

--- includes/Database.php
+++ includes/Database.php
@@ -7,7 +7,7 @@
 error_reporting(E_ALL);
         try {
             //$bd = 'pgsql:host=localhost;port=5432;dbname=ARCADIA;user=postgres;password=barrywhite92';
-            $bd = 'pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require';
+            $bd = 'pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require;options=\'-c endpoint=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech\'';
             # cat .env | grep DATABASE_URL
             //$bd="postgresql://neondb_owner:npg_KBeldUjMT7D5@ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech/ARCADIA?sslmode=require&charset=utf8";   
             $this->pdo = new PDO($bd);
