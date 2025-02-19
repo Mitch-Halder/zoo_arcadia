6b8fe7f2b57553f42e7b4f16ed825af71e2f2837
Mitch <halder.jean-michel@orange.fr>
2025-02-19 22:22:34 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -30,7 +30,7 @@ session_start();
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
         <!--<base href="/"> -->
         <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/css/style.css">
     </head>
    
     <body>
