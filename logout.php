4428bee27c8f644aa16bc2ce4f54ffa50238d2de
Mitch <halder.jean-michel@orange.fr>
2025-02-19 22:36:47 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,8 +3,6 @@ session_start();
 session_destroy();
 //$redirect=isset($_SESSION['redirect']) ? $_SESSION['redirect']:'/home.php';
 $redirect='home.php';
-print_r($redirect);
-unset($_SESSION['redirect']);
-header("Location: {$redirect}");
+header("Location: home.php");
 exit();
 ?>
