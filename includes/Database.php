9a6c542b06740d1eb3cf1e8ce26f85b7d21a5f13
Mitch <halder.jean-michel@orange.fr>
2025-02-19 22:15:25 +0100

    Update Database.php

--- includes/Database.php
+++ includes/Database.php
@@ -1,21 +1,25 @@
 <?php
-    class Database {
-        private static $instance = null;
-        private $pdo;
-        private function __construct() {
-            ini_set('display_errors', 1);
-error_reporting(E_ALL);
+class Database {
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct() {
         try {
-            //$bd = 'pgsql:host=localhost;port=5432;dbname=ARCADIA;user=postgres;password=barrywhite92';
-            $bd = 'pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;user=neondb_owner;password=npg_KBeldUjMT7D5;sslmode=require;endpoint=ep-noisy-queen-a8vi6crd-pooler';
-            # cat .env | grep DATABASE_URL
-            //$bd="postgresql://neondb_owner:npg_KBeldUjMT7D5@ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech/ARCADIA?sslmode=require&charset=utf8";   
-            $this->pdo = new PDO($bd);
+            $bd = "pgsql:host=ep-noisy-queen-a8vi6crd-pooler.eastus2.azure.neon.tech;port=5432;dbname=ARCADIA;sslmode=require";
+            $this->pdo = new PDO($bd, 'neondb_owner', 'npg_KBeldUjMT7D5');
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            if (!$this->pdo) {
+                throw new Exception('Impossible de se connecter à la base de données.');
+            }
+
         } catch (PDOException $e) {
             die("Erreur de connexion : " . $e->getMessage());
+        } catch (Exception $e) {
+            die("Erreur : " . $e->getMessage());
         }
     }
+
     public static function getInstance() {
         if (self::$instance === null) {
             self::$instance = new Database();
